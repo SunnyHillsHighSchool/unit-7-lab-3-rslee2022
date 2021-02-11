4ebebb0be2738d7976d3469721c1cc4c765b08ad
Rachel Lee <600021256@fjuhsd.org>
2021-02-11 08:45:13 +0000

    Unit 7 Lab 3. Method to return list of a number's factors excluding 1 and itself. 2nd method to remove prime numbers from list.

--- ArrayListFunHouse.java
+++ ArrayListFunHouse.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Rachel Lee
 
 import java.util.Scanner;
 import java.util.List;
@@ -12,10 +12,33 @@ public class ArrayListFunHouse
 {
  public static ArrayList<Integer> getListOfFactors(int number)
  {
-  return null;
+  //Instantiate arraylist of Integers called factorList
+  ArrayList<Integer> factorList = new ArrayList<Integer>();
+  //For loop: initialize loop control variable integer i to 2, condition - i is less than number (parameter), i increments by 1 each iteration
+  for(int i = 2; i < number; i++)
+  {
+    //if the remainder of the the number divided by i is equal to 0 (using modulus: %)
+    if(number % i == 0)
+      //add i to the arraylist factorList
+      factorList.add(i);
+  }
+  //Return arraylist factorList
+  return factorList;
  }
  
  public static void keepOnlyCompositeNumbers( List<Integer> nums )
  {
+   //instantiate arraylist of Integers called factors
+   ArrayList<Integer> factors = new ArrayList<Integer>();
+   //For loop: initialize loop control variable Integer i to the arraylist (parameter) nums’s size - 1, condition - i is greater than or equal to 0, have i decrement by 1
+   for(int i = nums.size()-1; i >= 0; i--)
+   {
+     //Set factors equal to getListOfFactors of the Integer at index i of nums
+     factors = getListOfFactors(nums.get(i));
+     //if the size of factors is equal to 0
+     if(factors.size() == 0)
+      //remove i from the arraylist nums
+      nums.remove(i);
+   }	
  }
 }
--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Rachel Lee
 
 import java.util.Arrays;
 import java.util.ArrayList;
